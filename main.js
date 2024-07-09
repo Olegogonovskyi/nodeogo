ff0878f6ffa8ad967325520c4a30966789dda24d
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-09 13:56:21 +0300

    less 2 change

--- main.js
+++ main.js
@@ -9,30 +9,27 @@ const fileType = '.txt'
 const whatIsThat = async (obPath) => {
     const stats = await fs.stat(obPath)
     if (stats.isFile()) {
-        console.log(`${__filename} є файлом шлях до нього -------- ${obPath}`)
+        console.log(`${path.basename(obPath)} ---- є файлом шлях до нього -------- ${obPath}`)
     } else if (stats.isDirectory()) {
-        console.log(`${path.dirname(obPath)} ---- є папкою шлях до неї -------- ${obPath}`)
+        console.log(`${path.basename(obPath)} ---- є папкою шлях до неї -------- ${obPath}`)
     } else {
-        console.log(`${__filename} ---- є фіг зна чим шлях до цього -------- ${obPath}`)
+        console.log(`${path.basename(obPath)} ---- є фіг зна чим шлях до цього -------- ${obPath}`)
     }
 }
 
 const domashka = async () => {
     await fs.mkdir(pathFolder, {recursive: true})
-    const absolutePath = await path.resolve(pathFolder);
-
+    const absolutePath = path.resolve(pathFolder);
+    await whatIsThat(absolutePath)
     for (let i = 0; i < folderArrays.length; i++) {
-        const pathToFolder = await `${absolutePath}\${folderArrays[i]}`
+        const pathToFolder = `${absolutePath}/${folderArrays[i]}`
         await fs.mkdir(pathToFolder, {recursive: true})
         await whatIsThat(pathToFolder)
         for (let j = 0; j < fileArrays.length; j++) {
-            const filePath = await path.join(pathToFolder, '/', `${fileArrays[j]}` + fileType)
+            const filePath = path.join(pathToFolder, '/', `${fileArrays[j]}${folderArrays[i]}` + fileType)
             await fs.writeFile(filePath, `Hello from ${folderArrays[i]} file that is in ${path.resolve(filePath)}`)
             await whatIsThat(filePath)
         }
-        // await fss.readFile(filePath, "utf-8", (err, data) => {
-        //     console.log('read -------', data)
-        // })
     }
 }
 void domashka()
