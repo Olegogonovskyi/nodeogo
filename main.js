c1b9f1de15ceef2ff8ea917b89f886e9ceb60022
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-09 11:14:42 +0300

    less 2 done

--- main.js
+++ main.js
@@ -1,14 +1,29 @@
-const arr = require('./sssss/arrmod')
-const express = require('express')
+const path = require('node:path')
+const fs = require("node:fs/promises")
 
-const app = express()
+const pathFolder = './baseFolder'
+const folderArrays = ['one', 'two', 'three', 'four', 'five']
+const fileType = '.txt'
 
-const PORT = 5300
+const domashka = async () => {
+    await fs.mkdir(pathFolder, {recursive: true})
+    const absolutePath = path.resolve(pathFolder);
+    for (let i = 0; i < folderArrays.length; i++) {
+        const pathToFolder = absolutePath + '/' + `${folderArrays[i]}`
+        await fs.mkdir(pathToFolder, {recursive: true})
 
-app.get('/', (req, res)=> {
-    res.json(arr)
-})
+        const filePath = path.join(pathToFolder, '/', `${folderArrays[i]}` + fileType)
+        await fs.writeFile(filePath, `Hello from ${folderArrays[i]} file`)
+        console.log(`Path to ${folderArrays[i]} file is ---------`, filePath)
+        const stats = await fs.stat(filePath)
+        if (stats.isFile()) {
+            console.log(`${filePath} .................є файлом.`)
+        } else if (stats.isDirectory()) {
+            console.log(`${filePath} ................. є папкою.`)
+        } else {
+            console.log(`${filePath} є фіг зна чим.`)
+        }
 
-app.listen(PORT, ()=> {
-    console.log(PORT)
-})
+    }
+}
+domashka()
