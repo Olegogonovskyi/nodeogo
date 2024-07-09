005b6b26cea90907a595b01dd7ace33d28b0a9d2
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-09 13:21:57 +0300

    less 2 change

--- less2.js
+++ less2.js
@@ -1,9 +1,22 @@
 const path = require('node:path')
 const fs = require("node:fs/promises")
+// const fss = require("node:fs")
 
 const pathFolder = './baseFolder'
-const folderArrays = ['one', 'two', 'three', 'four', 'five']
+const folderArrays = ['oneFolder', 'twoFolder', 'threeFolder', 'fourFolder', 'fiveFolder']
+const fileArrays = ['oneFile', 'twoFile', 'threeFile', 'fourFile', 'fiveFile']
 const fileType = '.txt'
+
+const whatIsThat = async (obPath) => {
+    const stats = await fs.stat(obPath)
+    if (stats.isFile()) {
+        console.log(`${obPath} є файлом.`)
+    } else if (stats.isDirectory()) {
+        console.log(`${obPath} є папкою.`)
+    } else {
+        console.log(`${obPath} є фіг зна чим.`)
+    }
+}
 
 const domashka = async () => {
     await fs.mkdir(pathFolder, {recursive: true})
@@ -11,19 +24,16 @@ const domashka = async () => {
     for (let i = 0; i < folderArrays.length; i++) {
         const pathToFolder = absolutePath + '/' + `${folderArrays[i]}`
         await fs.mkdir(pathToFolder, {recursive: true})
-
-        const filePath = path.join(pathToFolder, '/', `${folderArrays[i]}` + fileType)
-        await fs.writeFile(filePath, `Hello from ${folderArrays[i]} file`)
-        console.log(`Path to ${folderArrays[i]} file is ---------`, filePath)
-        const stats = await fs.stat(filePath)
-        if (stats.isFile()) {
-            console.log(`${filePath} є файлом.`)
-        } else if (stats.isDirectory()) {
-            console.log(`${filePath} є папкою.`)
-        } else {
-            console.log(`${filePath} є фіг зна чим.`)
+        await whatIsThat(pathToFolder)
+        // console.log(`Path to ${folderArrays[i]} is ---------`, filePath)
+        for (let j = 0; j < fileArrays.length; j++) {
+            const filePath = path.join(pathToFolder, '/', `${fileArrays[j]}` + fileType)
+            await fs.writeFile(filePath, `Hello from ${folderArrays[i]} file that is in ${path.resolve(filePath)}`)
+            await whatIsThat(filePath)
         }
-
+        // await fss.readFile(filePath, "utf-8", (err, data) => {
+        //     console.log('read -------', data)
+        // })
     }
 }
-domashka()
+void domashka()
