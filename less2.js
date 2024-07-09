b7bf461d91bfe1d10b5b309eefeb5fc1a681887f
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-09 14:46:43 +0300

    main play with fs

--- less2.js
+++ less2.js
@@ -1,6 +1,6 @@
 const path = require('node:path')
 const fs = require("node:fs/promises")
-// const fss = require("node:fs")
+const fss = require("node:fs")
 
 const pathFolder = './baseFolder'
 const folderArrays = ['oneFolder', 'twoFolder', 'threeFolder', 'fourFolder', 'fiveFolder']
@@ -10,30 +10,32 @@ const fileType = '.txt'
 const whatIsThat = async (obPath) => {
     const stats = await fs.stat(obPath)
     if (stats.isFile()) {
-        console.log(`${obPath} є файлом.`)
+        console.log(`${path.basename(obPath)} ---- є файлом шлях до нього -------- ${obPath}`)
     } else if (stats.isDirectory()) {
-        console.log(`${obPath} є папкою.`)
+        console.log(`${path.basename(obPath)} ---- є папкою шлях до неї -------- ${obPath}`)
     } else {
-        console.log(`${obPath} є фіг зна чим.`)
+        console.log(`${path.basename(obPath)} ---- є фіг зна чим шлях до цього -------- ${obPath}`)
     }
 }
 
 const domashka = async () => {
-    await fs.mkdir(pathFolder, {recursive: true})
-    const absolutePath = path.resolve(pathFolder);
-    for (let i = 0; i < folderArrays.length; i++) {
-        const pathToFolder = absolutePath + '/' + `${folderArrays[i]}`
-        await fs.mkdir(pathToFolder, {recursive: true})
-        await whatIsThat(pathToFolder)
-        // console.log(`Path to ${folderArrays[i]} is ---------`, filePath)
-        for (let j = 0; j < fileArrays.length; j++) {
-            const filePath = path.join(pathToFolder, '/', `${fileArrays[j]}` + fileType)
-            await fs.writeFile(filePath, `Hello from ${folderArrays[i]} file that is in ${path.resolve(filePath)}`)
-            await whatIsThat(filePath)
-        }
-        // await fss.readFile(filePath, "utf-8", (err, data) => {
-        //     console.log('read -------', data)
-        // })
-    }
+
+await fs.mkdir(path.join(__dirname, ...folderArrays), {recursive: true})
+    // await fs.mkdir(pathFolder, {recursive: true})
+    // const absolutePath = path.resolve(pathFolder);
+    // await whatIsThat(absolutePath)
+    // for (let i = 0; i < folderArrays.length; i++) {
+    //     const pathToFolder = `${absolutePath}/${folderArrays[i]}`
+    //     await fs.mkdir(pathToFolder, {recursive: true})
+    //     await whatIsThat(pathToFolder)
+    //     for (let j = 0; j < fileArrays.length; j++) {
+    //         const filePath = path.join(pathToFolder, '/', `${fileArrays[j]}${folderArrays[i]}` + fileType)
+    //         await fs.writeFile(filePath, `Hello from ${folderArrays[i]} file that is in ${path.resolve(filePath)}`)
+    //         await whatIsThat(filePath)
+    //         await fss.readFile(filePath, "utf-8", (err, data) => {
+    //             console.log(`${path.basename(filePath)} ----read----`, data)
+    //         })
+    //     }
+    // }
 }
 void domashka()
