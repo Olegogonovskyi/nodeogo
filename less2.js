acd17a3f7f2d8c79531178115c7cc73e28340b8e
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-09 11:10:17 +0300

    less 2 done

--- less2.js
+++ less2.js
@@ -1,11 +1,9 @@
 const path = require('node:path')
 const fs = require("node:fs/promises")
 
-console.log('dirname--------------', __dirname)
-console.log('dir + file------------', path.join(__dirname, __filename))
-
 const pathFolder = './baseFolder'
 const folderArrays = ['one', 'two', 'three', 'four', 'five']
+const fileType = '.txt'
 
 const domashka = async () => {
     await fs.mkdir(pathFolder, {recursive: true})
@@ -13,6 +11,19 @@ const domashka = async () => {
     for (let i = 0; i < folderArrays.length; i++) {
         const pathToFolder = absolutePath + '/' + `${folderArrays[i]}`
         await fs.mkdir(pathToFolder, {recursive: true})
+
+        const filePath = path.join(pathToFolder, '/', `${folderArrays[i]}` + fileType)
+        await fs.writeFile(filePath, `Hello from ${folderArrays[i]} file`)
+        console.log(`Path to ${folderArrays[i]} file is ---------`, filePath)
+        const stats = await fs.stat(filePath)
+        if (stats.isFile()) {
+            console.log(`${filePath} є файлом.`)
+        } else if (stats.isDirectory()) {
+            console.log(`${filePath} є папкою.`)
+        } else {
+            console.log(`${filePath} є фіг зна чим.`)
+        }
+
     }
 }
 domashka()
