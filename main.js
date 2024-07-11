652424c4f86c96bdc133a931de491e217e831743
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-11 10:55:37 +0300

    less-3-homework: done

--- main.js
+++ main.js
@@ -1,14 +1,102 @@
-const arr = require('./sssss/arrmod')
 const express = require('express')
+const path = require('node:path')
+const fs = require('node:fs/promises');
 
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-const PORT = 5300
+const PORT = 5200
+const pathToArray = path.join(__dirname, 'superheroes.txt')
+const superheroesPath = '/superheroes'
 
-app.get('/', (req, res)=> {
-    res.json(arr)
+const superHeroGetter = async () => {
+    const data = await fs.readFile(pathToArray, "utf-8");
+    return JSON.parse(data);
+}
+
+app.get(superheroesPath, async (req, res) => {
+    try {
+        const data = await superHeroGetter()
+        await res.json(data);
+    } catch (e) {
+        res.status(500).json({message: 'Друже, сервер на відпочинку'})
+    }
+
+})
+app.post(superheroesPath, async (req, res) => {
+    try {
+        const data = await superHeroGetter()
+        const addData = req.body
+        const duplicate = data.find(
+            (item) => item.id === addData.id || item.name === addData.name || item.image.url === addData.image.url
+        );
+        if (duplicate) {
+            res.status(409).json({message: 'Такі дані вже exsists'});
+        } else {
+            data.push(addData);
+            await fs.writeFile(pathToArray, JSON.stringify(data));
+            res.status(201).json({message: 'Bimba'});
+        }
+    } catch (e) {
+        res.status(400).json({message: 'Bad Request'})
+    }
+})
+app.delete('/superheroes/:id', async (req, res) => {
+    try {
+        const superID = +req.params.id
+        const data = await superHeroGetter()
+        const index = data.findIndex(superhero => superhero.id === superID)
+
+        index === -1 ? res.status(404).json({message: 'this superhero тут не проживає'}) : data.splice(index, 1)
+        await fs.writeFile(pathToArray, JSON.stringify(data))
+        res.status(204).json({message: 'this superhero deleted'})
+    } catch (e) {
+        res.status(400).json(e.message)
+    }
+})
+app.put('/superheroes/:id', async (req, res) => {
+    const superID = +req.params.id
+    const {id, name, image} = req.body
+    const data = await superHeroGetter()
+    const superhero = data.find(superhero => superhero.id === superID)
+    if (!superhero) {
+        return res.status(404).json({message: 'this superhero тут не проживає'})
+    }
+    if (id) {
+        const duplicate = data.find(
+            (item) => item.id === id
+        );
+        if (duplicate) {
+            res.status(409).json({message: 'Такі дані вже exsists'});
+        }
+        superhero.id = id
+
+    }
+    if (name) {
+        const duplicate = data.find(
+            (item) => item.name === name
+        );
+        if (duplicate) {
+            res.status(409).json({message: 'Такі дані вже exsists'});
+        }
+        superhero.name = name
+        res.status()
+    }
+    if (image) {
+        const duplicate = data.find(
+            (item) => item.id === image
+        );
+        if (duplicate) {
+            res.status(409).json({message: 'Такі дані вже exsists'});
+        }
+        superhero.image.url = image
+    }
+
+    await fs.writeFile(pathToArray, JSON.stringify(data));
+    res.status(201).json({message: 'Bimba'});
 })
 
-app.listen(PORT, ()=> {
+app.listen(PORT, () => {
     console.log(PORT)
 })
