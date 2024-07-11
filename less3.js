046538630ca5bb74203651dbec32569c21486518
Olegogonovskyi <Olegogonowskyi@gmail.com>
2024-07-11 10:15:35 +0300

    main play hw3

--- less3.js
+++ less3.js
@@ -32,15 +32,29 @@ app.post(superheroesPath, async (req, res) => {
             (item) => item.id === addData.id || item.name === addData.name || item.image.url === addData.image.url
         );
         if (duplicate) {
-            res.status(409).json({ message: 'Такі дані вже існують' });
+            res.status(409).json({message: 'Такі дані вже exsists'});
         } else {
             data.push(addData);
-            await fs.writeFile(pathToArray, JSON.stringify(data, null, 2));
-            res.status(201).json({ message: 'Bimba' });
+            await fs.writeFile(pathToArray, JSON.stringify(data));
+            res.status(201).json({message: 'Bimba'});
         }
     } catch (e) {
-res.status(400).json({message: 'Bad Request' })
+        res.status(400).json({message: 'Bad Request'})
     }
+})
+app.delete('/superheroes/:id',  async (req, res) => {
+    try {
+        const superID = +req.params.id
+        const data =  await superHeroGetter()
+        const index = data.findIndex(superhero => superhero.id === superID)
+
+        index === -1 ? res.status(404).json({message: 'this superhero тут не проживає'}) : data.splice(index, 1)
+        await fs.writeFile(pathToArray, JSON.stringify(data))
+        res.status(204).json({message: 'this superhero deleted'})
+    } catch (e) {
+        res.status(400).json(e.message)
+    }
+
 })
 
 app.listen(PORT, () => {
